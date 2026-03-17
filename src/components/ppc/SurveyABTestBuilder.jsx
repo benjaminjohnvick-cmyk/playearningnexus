2f8d2a68fcb02d72e79701df6741d049b3d7e321
benjamin vick <benjaminjohnvick@gmail.com>
2026-03-17 04:41:17 +0000

    File changes

--- src/components/ppc/SurveyABTestBuilder.jsx
+++ src/components/ppc/SurveyABTestBuilder.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
