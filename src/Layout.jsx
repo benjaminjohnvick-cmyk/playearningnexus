8b1b6716c166a61fa491975dbdca670ad5f3870a
benjamin vick <benjaminjohnvick@gmail.com>
2026-03-23 20:06:34 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -129,6 +129,7 @@ export default function Layout({ children, currentPageName }) {
     { name: 'PPC Marketplace', icon: TrendingUp, path: 'PPCMarketplace', requireAuth: true },
     { name: 'Payout History', icon: DollarSign, path: 'PayoutHistory', requireAuth: true },
     { name: 'My Payouts', icon: DollarSign, path: 'MyPayouts', requireAuth: true },
+    { name: 'My Orders', icon: ShoppingCart, path: 'MyOrders', requireAuth: true },
     { name: 'Campaigns', icon: TrendingUp, path: 'Campaigns', requireAuth: true },
     { name: 'Notifications', icon: Settings, path: 'NotificationHistory', requireAuth: true },
     { name: 'Challenges', icon: Trophy, path: 'Challenges', requireAuth: true },
